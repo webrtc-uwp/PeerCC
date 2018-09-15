9a654cb72760fbeef3f05bf9e62f7b96432bd92b
Vladimir Morosev <mosa@gnedo.com>
2018-09-15 02:12:45 +0200

    raycasting issues fixed

--- ClientUnity/Assets/Scripts/GazeGestureManager.cs
+++ ClientUnity/Assets/Scripts/GazeGestureManager.cs
@@ -8,7 +8,6 @@ public class GazeGestureManager : MonoBehaviour
 {
     public static GazeGestureManager Instance { get; private set; }
 
-    // Represents the hologram that is currently being gazed at.
     public GameObject FocusedObject { get; private set; }
 
     GestureRecognizer recognizer;
@@ -16,16 +15,13 @@ public class GazeGestureManager : MonoBehaviour
     public Button ConnectButton;
     public Button CallButton;
 
-    // Use this for initialization
     void Start()
     {
         Instance = this;
 
-        // Set up a GestureRecognizer to detect Select gestures.
         recognizer = new GestureRecognizer();
         recognizer.Tapped += (args) =>
         {
-            // Send an OnSelect message to the focused object and its ancestors.
             if (FocusedObject != null)
             {
                 if (FocusedObject.GetComponent<Button>() == ConnectButton)
@@ -41,58 +37,36 @@ public class GazeGestureManager : MonoBehaviour
         recognizer.StartCapturingGestures();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Figure out which hologram is focused this frame.
         GameObject oldFocusObject = FocusedObject;
 
-        // Do a raycast into the world based on the user's
-        // head position and orientation.
         var headPosition = Camera.main.transform.position;
         var gazeDirection = Camera.main.transform.forward;
 
         RaycastHit hitInfo;
-        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo, 20.0f, Physics.DefaultRaycastLayers))
         {
-            // If the raycast hit a hologram, use that as the focused object.
             FocusedObject = hitInfo.collider.gameObject;
-        }
-        else
-        {
-            PointerEventData pointerData = new PointerEventData(EventSystem.current);
-            pointerData.position = new Vector2(headPosition.x, headPosition.y);
-            List<RaycastResult> objects = new List<RaycastResult>();
-            EventSystem.current.RaycastAll(pointerData, objects);
-            if (objects.Count > 0)
+
+            if (hitInfo.collider.gameObject.GetComponent<Button>() == ConnectButton)
             {
-                bool buttonGazed = false;
-                foreach (RaycastResult result in objects)
-                {
-                    if (result.gameObject.GetComponent<Button>() == ConnectButton)
-                    {
-                        buttonGazed = true;
-                        FocusedObject = result.gameObject;
-                    }
-                    else if (result.gameObject.GetComponent<Button>() == CallButton)
-                    {
-                        buttonGazed = true;
-                        FocusedObject = result.gameObject;
-                    }
-                    System.Diagnostics.Debug.WriteLine(result.gameObject.name + " " + this.transform.position);
-                }
-                if (!buttonGazed)
-                    FocusedObject = null;
+                FocusedObject = hitInfo.collider.gameObject;
+            }
+            else if (hitInfo.collider.gameObject.GetComponent<Button>() == CallButton)
+            {
+                FocusedObject = hitInfo.collider.gameObject;
             }
             else
             {
-                // If the raycast did not hit a hologram, clear the focused object.
                 FocusedObject = null;
             }
         }
+        else
+        {
+            FocusedObject = null;
+        }
 
-        // If the focused object changed this frame,
-        // start detecting fresh gestures again.
         if (FocusedObject != oldFocusObject)
         {
             recognizer.CancelGestures();
--- ClientUnity/Assets/Scripts/WorldCursor.cs
+++ ClientUnity/Assets/Scripts/WorldCursor.cs
@@ -9,96 +9,74 @@ public class WorldCursor : MonoBehaviour
 
     public Button ConnectButton;
     public Button CallButton;
+    public Camera MainCamera;
 
     private bool connectButtonSelected = false;
     private bool callButtonSelected = false;
 
-    // Use this for initialization
     void Start()
     {
-        // Grab the mesh renderer that's on the same object as this script.
         meshRenderer = this.gameObject.GetComponentInChildren<MeshRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Do a raycast into the world based on the user's
-        // head position and orientation.
-        var headPosition = Camera.main.transform.position;
-        var gazeDirection = Camera.main.transform.forward;
-
+        var headPosition = MainCamera.transform.position;
+        var gazeDirection = MainCamera.transform.forward;
+        Debug.DrawRay(headPosition, gazeDirection * 10.0f, Color.red);
         RaycastHit hitInfo;
-
-        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+       
+        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo, 20.0f, Physics.DefaultRaycastLayers))
         {
-            // If the raycast hit a hologram...
-            // Display the cursor mesh.
+            this.transform.position = new Vector3(hitInfo.point.x, hitInfo.point.y, -0.05f);
+            this.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
             meshRenderer.enabled = true;
 
-            // Move thecursor to the point where the raycast hit.
-            this.transform.position = hitInfo.point;
+            System.Diagnostics.Debug.WriteLine("WorldCursor - Raycast - objects hit");
 
-            // Rotate the cursor to hug the surface of the hologram.
-            this.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+            if (hitInfo.collider.gameObject.GetComponent<Button>() == ConnectButton)
+            {
+                if (!connectButtonSelected)
+                {
+                    connectButtonSelected = true;
+                    ColorBlock colorBlock = ConnectButton.colors;
+                    colorBlock.normalColor = Color.red;
+                    ConnectButton.colors = colorBlock;
+                }
+            }
+            else if (hitInfo.collider.gameObject.GetComponent<Button>() == CallButton)
+            {
+                if (!callButtonSelected)
+                {
+                    callButtonSelected = true;
+                    ColorBlock colorBlock = CallButton.colors;
+                    colorBlock.normalColor = Color.red;
+                    CallButton.colors = colorBlock;
+                }
+            }
+            else if (connectButtonSelected || callButtonSelected)
+            {
+                ResetButtons();
+            }
         }
-        else
+        else if (connectButtonSelected || callButtonSelected)
         {
-            PointerEventData pointerData = new PointerEventData(EventSystem.current);
-            pointerData.position = new Vector2(headPosition.x, headPosition.y);
-            List<RaycastResult> objects = new List<RaycastResult>();
-            EventSystem.current.RaycastAll(pointerData, objects);
-            if (objects.Count > 0)
-            {
-                bool buttonGazed = false;
-                GameObject firstGameObject = null;
-                System.Diagnostics.Debug.WriteLine("WorldCursor - RaycastAll objects hit");
-                foreach (RaycastResult result in objects)
-                {
-                    if (firstGameObject == null)
-                        firstGameObject = result.gameObject;
-                    if (result.gameObject.GetComponent<Button>() == ConnectButton)
-                    {
-                        connectButtonSelected = true;
-                        buttonGazed = true;
-                        ColorBlock colorBlock = ConnectButton.colors;
-                        colorBlock.normalColor = Color.red;
-                        ConnectButton.colors = colorBlock;
-                    }
-                    else if (result.gameObject.GetComponent<Button>() == CallButton)
-                    {
-                        callButtonSelected = true;
-                        buttonGazed = true;
-                        ColorBlock colorBlock = CallButton.colors;
-                        colorBlock.normalColor = Color.red;
-                        CallButton.colors = colorBlock;
-                    }
-                }
-                if (!buttonGazed)
-                {
-                    if (connectButtonSelected)
-                    {
-                        connectButtonSelected = false;
-                        ColorBlock colorBlock = ConnectButton.colors;
-                        colorBlock.normalColor = Color.white;
-                        ConnectButton.colors = colorBlock;
-                    }
-                    if (callButtonSelected)
-                    {
-                        callButtonSelected = false;
-                        ColorBlock colorBlock = CallButton.colors;
-                        colorBlock.normalColor = Color.white;
-                        CallButton.colors = colorBlock;
-                    }
-                }
-                this.transform.position = new Vector3(firstGameObject.transform.position.x, firstGameObject.transform.position.y, -40);
-                meshRenderer.enabled = true;
-            }
-            else
-            {
-                // If the raycast did not hit a hologram, hide the cursor mesh.
-                meshRenderer.enabled = false;
-            }
+            meshRenderer.enabled = false;
+
+            ResetButtons();
         }
+    }
+
+    private void ResetButtons()
+    {
+        connectButtonSelected = false;
+        ColorBlock colorBlock = ConnectButton.colors;
+        colorBlock.normalColor = Color.white;
+        ConnectButton.colors = colorBlock;
+
+        callButtonSelected = false;
+        colorBlock = CallButton.colors;
+        colorBlock.normalColor = Color.white;
+        CallButton.colors = colorBlock;
     }
 }
