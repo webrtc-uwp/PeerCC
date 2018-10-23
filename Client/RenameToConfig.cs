ed3918f3e14a916cbb29c9723d50c265550fc61b
ivanab <ivana@opticaltone.com>
2018-10-23 16:04:22 +0200

    Added RenameToConfig class

---
+++ Client/RenameToConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Windows.Storage;
+
+namespace PeerConnectionClient
+{
+    // Rename RenameToConfig class to Config and add values
+    public static class RenameToConfig
+    {
+        public static ApplicationDataContainer localSettings =
+                ApplicationData.Current.LocalSettings;
+
+        public static void AppSettings()
+        {
+            localSettings.Values["appID"] = "";
+            localSettings.Values["keyID"] = "";
+
+            Random rnd = new Random();
+            int rndnum = rnd.Next();
+            localSettings.Values["confID"] = rndnum.ToString();
+
+            // secret string
+            localSettings.Values["secret"] = "";
+        }
+    }
+}
