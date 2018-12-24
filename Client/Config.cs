4a18a5686b44a37e61e53510bde01c96c150f9fe
ivanab <ivana@opticaltone.com>
2018-12-24 14:33:51 +0100

    Removed RenameToConfig class, added Config class

---
+++ Client/Config.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using Windows.Storage;
+
+namespace PeerConnectionClient
+{
+    public static class Config
+    {
+        public static ApplicationDataContainer localSettings =
+                ApplicationData.Current.LocalSettings;
+
+        public static void AppSettings()
+        {
+            string conf = @"config.txt";
+            Dictionary<string, string> dict;
+
+
+            if (!File.Exists(conf))
+            {
+                localSettings.Values["appID"] = "";
+                localSettings.Values["keyID"] = "";
+
+                // secret string
+                localSettings.Values["secret"] = "";
+            }
+            else
+            {
+                dict = File.ReadAllLines("config.txt")
+                .Select(l => l.Split(new[] { '=' }))
+                .ToDictionary(s => s[0].Trim(), s => s[1].Trim());
+
+                localSettings.Values["appID"] = dict["appID"];
+                localSettings.Values["keyID"] = dict["keyID"];
+                localSettings.Values["secret"] = dict["secret"];
+            }
+
+            Random rnd = new Random();
+            int rndnum = rnd.Next();
+            localSettings.Values["confID"] = rndnum.ToString();
+
+            localSettings.Values["userID"] = GetLocalPeerName();
+            localSettings.Values["localID"] = GetLocalPeerName();
+        }
+
+        public static string GetLocalPeerName() =>
+            IPGlobalProperties.GetIPGlobalProperties().HostName?.ToLower() ?? "<unknown host>";
+    }
+}
